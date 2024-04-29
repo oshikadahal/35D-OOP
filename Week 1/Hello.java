a839e20ef7f93255cd276f005b8ec061b659a1c5
oshika <oshikadahal61@gmail.com>
2024-04-29 14:07:43 +0545

    jumpstatement

--- Week 1/Hello.java
+++ Week 1/Hello.java
@@ -1,6 +1,39 @@
 public class Hello{
     public static void main(String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+
+ */
         System.out.println("Hello World!");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output */
+      System.out.print("This is my first program \n I am on module ST4003CEM" );
+      
+      /*3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as "*/
+
+System.out.println("A\"quoted\" String is \n \'much\' better if you learn \n the rules of \"escape sequences.\" \n Also,\"\" represents an empty \n String. Don't forget: use \" \n instead of \" ! \n \'\' is not same as \"\"");
+
+
+
+
     }
 
 }
